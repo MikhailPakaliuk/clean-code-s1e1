3ac32fff7a5da13b33b62288399c0b3e9b0eaddd
MikhailPakaliuk <mr.dyurilen@mail.ru>
2022-05-31 11:15:54 +0300

    fix: rewrote tag selector styles and using meaningful class names according to rule 3.4 and 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -18,32 +18,38 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.className='list-item';
+    listItem.classList.add('list-item');
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add('item-input');
     //label
     var label=document.createElement("label");//label
+    label.classList.add('item-label');
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add('item-input');
     //button.edit
     var editButton=document.createElement("button");//edit button
+    editButton.classList.add('button');
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
+    deleteButton.classList.add('button');
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.classList.add('button-image');
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add('task');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add('task');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add('edit');
 
-    deleteButton.className="delete";
+    deleteButton.classList.add('delete');
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,6 +90,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
+    label.classList.add('item-label');
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
